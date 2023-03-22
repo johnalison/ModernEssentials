1a5e415800c082fbd13007561509bcaae9258b54
John Alison <johnda102@gmail.com>
2023-03-22 08:47:16 -0400

    Update

--- Grades/makeGrades.sh
+++ Grades/makeGrades.sh
@@ -1,1 +1,1 @@
-awk '{gsub(",","",$1); print "grades[\"" $1 "\"] = ("  $3 ")" }' grades.txt 
+awk '{gsub(",","",$1); print "grades[\"" $1 "\"] = ("  $3 "," $4 ")" }' grades.txt 
--- Grades/plotGrades.py
+++ Grades/plotGrades.py
@@ -6,40 +6,80 @@ import matplotlib
 from grades import grades as grades
 print(grades)
 
-scores = []
+midTerm1 = []
+midTerm2 = []
 
 for n in grades:
     print(n,grades[n])
-    scores.append(float(grades[n]))
+    _midTerm1, _midTerm2 = grades[n]
+    midTerm1.append(float(_midTerm1))
+    midTerm2.append(float(_midTerm2))
+
+midTerm1 = np.array(midTerm1)
+midTerm2 = np.array(midTerm2)
+    
+def convertToPercent(scores, totalPoints):
+    return np.array(scores)/totalPoints*100    
+
+def getAveRMS(grades):
+    return np.average(grades), np.std(grades)
+
+#print(np.array(midTerm1))
+#print(len(midTerm1))
+combinedMidTerms = midTerm1 + midTerm2
+
+midTerm1Percent = convertToPercent(midTerm1, 90)
+midTerm2Percent = convertToPercent(midTerm2, 90)
+combinedPercent = convertToPercent(combinedMidTerms, 90+90)
+
+print(grades.keys())
+for n, g in zip(grades.keys(),combinedPercent):
+    print(n,"\t",round(g,1))
+
+midterm1_ave, midterm1_rms = getAveRMS(midTerm1Percent)
+midterm2_ave, midterm2_rms = getAveRMS(midTerm2Percent)
+combined_ave, combined_rms = getAveRMS(combinedPercent)
 
 
-print(np.array(scores))
-print(len(scores))
-scoresPercent = np.array(scores)/90*100
-print(scoresPercent)
-midterm1_ave = round(np.average(scoresPercent),1)
-midterm1_rms = round(np.std(scoresPercent),1)
+print("MidTerm 1")
+print(f"\t{midterm1_ave}, {midterm1_rms}")
 
-print(midterm1_ave, midterm1_rms)
-
-bins = np.linspace(0,100,25)
-#print( bins)
-xMin = 20
+print("MidTerm 2")
+print(f"\t{midterm2_ave}, {midterm2_rms}")
 
 
-plt.hist(scoresPercent, bins=bins,color='b', alpha=0.65, histtype='stepfilled', label='stepfilled hist')
-plt.plot([midterm1_ave,midterm1_ave],[0,3],'r--')
-plt.annotate("Mean = "+str(midterm1_ave), xy=(xMin+2, 2.5), xytext=(xMin+2,4),
-             color="red", weight="light", fontsize=14,
-             #arrowprops={"facecolor": "red"}
-             )
-plt.annotate("RMS ="+str(midterm1_rms), xy=(xMin+2, 2.5), xytext=(xMin+2,3),
-             color="red", weight="light", fontsize=14,
-             #arrowprops={"facecolor": "red"}
-             )
-plt.xlim(0, 100)
-plt.ylim(0, 8)
-plt.xlabel("Midterm 1 Grades (%)")
-plt.savefig("MidTerm1.pdf")
-plt.savefig("MidTerm1.png")
-plt.close()
+
+def plot1D(name, grades, ave, rms, bins = np.linspace(0,100,25), xMin = 20):
+
+    plt.hist(grades, bins=bins,color='b', alpha=0.65, histtype='stepfilled', label='stepfilled hist')
+    plt.plot([ave,ave],[0,3],'r--')
+    plt.annotate("Mean = "+str(round(ave,1)), xy=(xMin+2, 2.5), xytext=(xMin+2,4),
+                 color="red", weight="light", fontsize=14,
+                 )
+    plt.annotate("RMS ="+str(round(rms,1)), xy=(xMin+2, 2.5), xytext=(xMin+2,3),
+                 color="red", weight="light", fontsize=14,
+                 )
+    plt.xlim(50, 100)
+    plt.ylim(0, 8)
+    plt.xlabel(name+" Grades (%)")
+    plt.savefig(name+".pdf")
+    plt.savefig(name+".png")
+    plt.close()
+
+plot1D("MidTerm1", midTerm1Percent, midterm1_ave, midterm1_rms)
+plot1D("MidTerm2", midTerm2Percent, midterm2_ave, midterm2_rms)
+plot1D("CombinedGrade", combinedPercent, combined_ave, combined_rms, bins=np.linspace(0,100,50))
+
+
+
+
+plt.scatter(midTerm1Percent, midTerm2Percent, color='b') #, label='') 
+plt.xlim(0, 100) 
+plt.ylim(0, 100) 
+plt.xlabel('Midterm 1') 
+plt.ylabel('Midterm 2') 
+#plt.legend() 
+plt.savefig('midTerm1_vs_midTerm2.pdf')
+plt.savefig('midTerm1_vs_midTerm2.png') 
+
+#plt.hist(grades, bins=bins,color='b', alpha=0.65, histtype='stepfilled', label='stepfilled hist')
